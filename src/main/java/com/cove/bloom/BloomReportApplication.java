423e46189a4e6195ea18881deb31ae3aac13a789
lja9207 <3wksu2@hanmail.net>
2024-02-23 09:55:40 +0900

    Initial commit

---
+++ src/main/java/com/cove/bloom/BloomReportApplication.java
@@ -0,0 +1,17 @@
+package com.cove.bloom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloomReportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloomReportApplication.class, args);
+
+        System.out.println("report ");
+    }
+
+
+
+}
